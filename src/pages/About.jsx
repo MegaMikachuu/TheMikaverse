f4cb44734224e4fb34fe35449716956d15518356
Reesejones1989 <48375855+Reesejones1989@users.noreply.github.com>
2025-06-29 17:21:56 -0400

    Fixed Shop page and updated About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,14 @@ export default function About() {
     return (
         <div className="about">
           <h1>About MikaChu</h1>
-          <h3>Welcome to the starscape</h3>
+          <h3>Welcome to the starscape
+
+          # TheMikaverse
+        The Mikaverse! 🚀✨
+
+      Welcome to THE MIKAVERSE — where stardust meets hustle, and magic meets mayhem.  
+      Created by Mika, the Sagittarius queen born from chaos and Nebula Nectar ♐, this site is your portal to stories, spells, and sagas from the multiverse itself.
+          </h3>
         </div>
         
     )
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -3,7 +3,10 @@ import './Shop.css';
 export default function Shop() {
   return (
     <div className="shop-page">
-       
+      <div className="shop-overlay">
+        <h1> Welcome to the Cosmic Shop</h1>
+        <p>Enchanting items and mystical artifacts await!</p>
+      </div>
     </div>
   );
 }
